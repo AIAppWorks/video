7e185350d5cc024da1b4cfef0f101957b16c1927
wang <641257395@qq.com>
2026-04-16 08:17:23 +0800

    增加导出，最后一帧图片

--- scripts/render-scene.sh
+++ scripts/render-scene.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 SCENE=${1:-1}
+
 npx remotion render src/index.ts model-names out/Scene${SCENE}.mp4 \
   --muted --overwrite \
   --audio-codec=aac \
   --codec=h264 --crf=23 \
   --image-format=jpeg --jpeg-quality=80 \
+  --props="{\"scene\":${SCENE}}"
+
+npx remotion still src/index.ts model-names out/img/Scene${SCENE}-last.jpg \
+  --frame=179 \
+  --jpeg-quality=90 \
   --props="{\"scene\":${SCENE}}"
